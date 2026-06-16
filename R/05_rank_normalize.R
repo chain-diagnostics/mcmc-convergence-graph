18473c07df369b2bea1c1d5d23004f23d06f6ceb
Cici Chen Gu <chen.gu@it.uu.se>
2026-06-16 13:52:11 +0200

    use styler::style_pkg() to style

--- R/05_rank_normalize.R
+++ R/05_rank_normalize.R
@@ -1,5 +1,5 @@
-#converts raw values into rank-normalized values
-rank_normalize <-function(x){
+# converts raw values into rank-normalized values
+rank_normalize <- function(x) {
   n <- length(x)
   ranks <- rank(x, ties.method = "average")
   stats::qnorm((ranks - 3 / 8) / (n + 1 / 4))
--- R/06_split_rhat.R
+++ R/06_split_rhat.R
@@ -1,11 +1,13 @@
-split_rhat <- function(split_draws){
+split_rhat <- function(split_draws) {
   n_iter <- nrows(split_draws)
   n_chains <- ncol(split_draws)
-  chain_means <-colMeans(split_draws)
-  chain_vars <- apply(split_draws,2,function(x){sum((x-mean(x)^2)/(length(x)-1))})
+  chain_means <- colMeans(split_draws)
+  chain_vars <- apply(split_draws, 2, function(x) {
+    sum((x - mean(x)^2) / (length(x) - 1))
+  })
   within_chain_var <- mean(chain_vars)
   overall_mean <- mean(chain_means)
-  between_chain_car <- n_iter*sum((chain_means-overall_mean)^2/(n_chains-1))
-  var_hat <-((n_iter-1)/n_iter)*within-chain_var+(1/n_iter)*between_chain_car
-  sqrt(var_hat/within_chain_var)
-  }
+  between_chain_car <- n_iter * sum((chain_means - overall_mean)^2 / (n_chains - 1))
+  var_hat <- ((n_iter - 1) / n_iter) * within - chain_var + (1 / n_iter) * between_chain_car
+  sqrt(var_hat / within_chain_var)
+}
