6863a1e0cafd190c0f452d502aba1723c63926be
Cici Chen Gu <chen.gu@it.uu.se>
2026-06-23 16:53:27 +0200

    changed the object name and documentation

--- R/05_rank_normalize.R
+++ R/05_rank_normalize.R
@@ -1,6 +1,20 @@
-# converts raw values into rank-normalized values
+#' Rank-normalize numeric draws
+#'
+#' Converts a numeric vector of draws into rank-normalized values. The raw values
+#' are first replaced by their ranks, and the ranks are transformed to approximate
+#' standard normal scores.
+#'
+#' @param x A numeric vector of draws
+#'
+#' @return A numeric vector of rank-normalized values with the same length as 'x'
+#'
+#' @keywords internal
+#'
 rank_normalize <- function(x) {
   n <- length(x)
   ranks <- rank(x, ties.method = "average")
-  stats::qnorm((ranks - 3 / 8) / (n + 1 / 4))
+  normalized_values <- stats::qnorm(
+    (ranks - 3 / 8) / (n + 1 / 4)
+  )
+  normalized_values
 }
