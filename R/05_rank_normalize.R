6912f77ba9c3e42821a9690005a90a55fa7f1003
Cici Chen Gu <chen.gu@it.uu.se>
2026-07-07 13:29:16 +0200

    updated the rank normalize with more error catch

--- R/05_rank_normalize.R
+++ R/05_rank_normalize.R
@@ -11,10 +11,18 @@
 #' @keywords internal
 
 rank_normalize <- function(x) {
+  if (!is.numeric(x)) {
+    stop("`x` must be a numeric vector.", call. = FALSE)
+  }
+  if (length(x) < 2) {
+    stop("`x` must contain at least 2 draws.", call. = FALSE)
+  }
+  if (anyNA(x)) {
+    stop("`x` must not contain missing values.", call. = FALSE)
+  }
   n <- length(x)
   ranks <- rank(x, ties.method = "average")
-  normalized_values <- stats::qnorm(
+  stats::qnorm(
     (ranks - 3 / 8) / (n + 1 / 4)
   )
-  normalized_values
 }
