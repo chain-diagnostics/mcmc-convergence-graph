a529e9f3fbc7017c02f11c617172d6514b77decc
Cici Chen Gu <chen.gu@it.uu.se>
2026-09-22 17:09:19 +0200

    checking point 09.22

--- R/07_pairwise_rhat_calculation.R
+++ R/07_pairwise_rhat_calculation.R
@@ -27,30 +27,27 @@ pairwise_rhat_calculation <- function(chain_i_draws, chain_j_draws) {
       call. = FALSE
     )
   }
-  pooled_draws <- c(chain_i_draws, chain_j_draws)
-  rank_draws <- rank_normalize(pooled_draws)
-  rank_chain_i <- rank_draws[seq_len(n_iter_i)]
-  rank_chain_j <- rank_draws[(n_iter_i + 1):(n_iter_i + n_iter_j)]
-  split_i <- split_chain(rank_chain_i)
-  split_j <- split_chain(rank_chain_j)
-  rank_split_draws <- cbind(
+  split_i <- split_chain(chain_i_draws)
+  split_j <- split_chain(chain_j_draws)
+  split_draws <- cbind(
     split_i$first,
     split_i$second,
     split_j$first,
     split_j$second
   )
+  rank_z <- rank_normalize(as.vector(split_draws))
+  rank_split_draws <- matrix(
+    rank_z,
+    nrow = nrow(split_draws),
+    ncol = 4
+  )
   rank_rhat <- split_rhat(rank_split_draws)
-  folded_draws <- abs(pooled_draws - stats::median(pooled_draws))
-  folded_rank_draws <- rank_normalize(folded_draws)
-  folded_rank_chain_i <- folded_rank_draws[seq_len(n_iter_i)]
-  folded_rank_chain_j <- folded_rank_draws[(n_iter_i + 1):(n_iter_i + n_iter_j)]
-  folded_split_i <- split_chain(folded_rank_chain_i)
-  folded_split_j <- split_chain(folded_rank_chain_j)
-  folded_split_draws <- cbind(
-    folded_split_i$first,
-    folded_split_i$second,
-    folded_split_j$first,
-    folded_split_j$second
+  folded_draws <- abs(split_draws - stats::median(split_draws))
+  folded_z <- rank_normalize(as.vector(folded_draws))
+  folded_split_draws <- matrix(
+    folded_z,
+    nrow = nrow(split_draws),
+    ncol = 4
   )
   folded_rhat <- split_rhat(folded_split_draws)
   max(rank_rhat, folded_rhat)
