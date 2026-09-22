cbc40ca8c32e43d781822c7ad615093c19698493
Cici Chen Gu <chen.gu@it.uu.se>
2026-09-22 17:10:29 +0200

    checking point 09.22

--- R/07_pairwise_rhat_calculation.R
+++ R/07_pairwise_rhat_calculation.R
@@ -1,8 +1,8 @@
 #' Compute pairwise R-hat for two chains
 #'
-#' Computes the pairwise rank-normalized split R-hat statistic for two chains of
-#' posterior draws from one parameter. The function also computes the folded
-#' split R-hat and returns the larger of the rank-normalized and folded values.
+#' Computes the pairwise rank-normalized split R-hat and rank-normalised
+#' split-R-hat statistic for two chains of posterior draws from one parameter,
+#' and returns the larger of the rank-normalized and folded values.
 #'
 #' @param chain_i_draws A numeric vector of posterior draws from the first chain.
 #' @param chain_j_draws A numeric vector of posterior draws from the second chain.
@@ -18,6 +18,7 @@
 #' chain_2 <- rnorm(100)
 #'
 #' pairwise_rhat_calculation(chain_1, chain_2)
+#'
 pairwise_rhat_calculation <- function(chain_i_draws, chain_j_draws) {
   n_iter_i <- length(chain_i_draws)
   n_iter_j <- length(chain_j_draws)
@@ -27,7 +28,8 @@ pairwise_rhat_calculation <- function(chain_i_draws, chain_j_draws) {
       call. = FALSE
     )
   }
-  split_i <- split_chain(chain_i_draws)
+  #split chains
+  split_i <- split_chain(chain_i_draws).
   split_j <- split_chain(chain_j_draws)
   split_draws <- cbind(
     split_i$first,
@@ -35,6 +37,7 @@ pairwise_rhat_calculation <- function(chain_i_draws, chain_j_draws) {
     split_j$first,
     split_j$second
   )
+  #calculate rank-normalised split-R-hat
   rank_z <- rank_normalize(as.vector(split_draws))
   rank_split_draws <- matrix(
     rank_z,
@@ -42,6 +45,7 @@ pairwise_rhat_calculation <- function(chain_i_draws, chain_j_draws) {
     ncol = 4
   )
   rank_rhat <- split_rhat(rank_split_draws)
+  #calculate rank-normalised folded-split-R-hat
   folded_draws <- abs(split_draws - stats::median(split_draws))
   folded_z <- rank_normalize(as.vector(folded_draws))
   folded_split_draws <- matrix(
@@ -50,5 +54,6 @@ pairwise_rhat_calculation <- function(chain_i_draws, chain_j_draws) {
     ncol = 4
   )
   folded_rhat <- split_rhat(folded_split_draws)
+  #get the larger of two number
   max(rank_rhat, folded_rhat)
 }
